54452bef528f901edab0a1baf9ec4aba31daa7c2
RickyTickyTavy <hangoverrick92@gmail.com>
2018-09-13 08:02:32 +0300

    upd

--- www/index.php
+++ www/index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- <meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
 	<title>megatemplateGulpSCSS</title>
 	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE"/>
 	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1"/>
